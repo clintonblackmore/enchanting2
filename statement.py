ac33ed239342fa5c1244150a66105e524ad94219
Clinton Blackmore <clinton.blackmore@gmail.com>
2014-04-19 09:07:47 -0600

    Simple version works

---
+++ statement.py
@@ -0,0 +1,186 @@
+
+import struct
+import array
+
+
+
+
+class CodeStream:
+    "A stream of data representing code that the execution engine runs."
+    
+    def __init__(self, buffer = 0):
+        if buffer != 0:
+            self.buffer = buffer
+        else:
+            self.buffer = array.array("B")
+        self.offset = 0
+        
+    def read_formatted_data(self, fmt):
+        offset = self.offset
+        self.offset += struct.calcsize(fmt)
+        return struct.unpack_from(fmt, self.buffer, offset)
+
+    def read_byte(self):
+        return self.read_formatted_data("!B")[0]        
+        
+    def read_short(self):
+        return self.read_formatted_data("!H")[0]
+
+    def read_bool(self):
+        return self.read_formatted_data("!?")[0]
+        
+    def read_float(self):
+        return self.read_formatted_data("!f")[0]
+        
+    def read_statement(self):
+        type_id = self.read_byte()
+        type_class = statement_type[type_id]
+        new_statement = type_class()
+        new_statement.read_from_stream(self)
+        return new_statement
+   
+
+    def write_formatted_data(self, fmt, *args):
+        offset = self.offset
+        self.offset += struct.calcsize(fmt)
+        struct.pack_into(fmt, self.buffer, offset, *args)
+
+    def write_byte(self, value):
+        self.write_formatted_data("!B", value);       
+        
+    def write_short(self, value):
+        self.write_formatted_data("!H", value);       
+
+    def write_bool(self, value):
+        self.write_formatted_data("!?", value); 
+        
+    def write_float(self, value):
+        self.write_formatted_data("!f", value);       
+        
+    def write_statement(self, statement):
+        type_class = statement.__class__
+        type_id = id_from_statement[type_class]
+        self.write_byte(type_id)
+        statement.write_to_stream(self)
+        
+
+class Statement:
+    "A statement is a one or more pieces of data or code, and the base class for such"
+    
+    def evaluate(self):
+        """Perform necessary steps to reduce this to a single data type.
+        For example, if it is an addition function, add the inputs and return a number"""
+        return self;
+    
+    def as_number(self):
+        return 0
+    
+    def as_bool(self):
+        return false
+    
+    def as_string(self):
+        return ""
+    
+    def read_from_stream(self, stream):
+        pass
+    
+    def write_to_stream(self, stream):
+        pass
+
+class NilStatement(Statement):
+    "A value that indicates no value"
+
+    def __repr__(self):
+        return 'Nil' 
+    
+    def read_from_stream(self, stream):
+        # Nil statement requires no additional information
+        pass
+
+    # as_number, as_bool, and as_string are as in the base class
+
+class NumberStatement(Statement):
+    "A value that indicates no value"
+
+    def __init__(self, value = 0):
+        self.value = value
+
+    def __repr__(self):
+        return "#" + str(self.value)
+
+    def read_from_stream(self, stream):
+        self.value = stream.read_float()
+
+    def as_number(self):
+        return self.value;
+    
+    def as_bool(self):
+        return self.value == 0.0;
+    
+    def as_string(self):
+        return str(self.value);
+
+    def read_from_stream(self, stream):
+        self.value = stream.read_float()
+    
+    def write_to_stream(self, stream):
+        stream.write_float(self.value)
+
+class FunctionStatment(Statement):
+    "A statement that represents a function"
+
+    operations = {
+        0: ["+", lambda x, y: x + y], 
+        1: ["-", lambda x, y: x - y], 
+        2: ["*", lambda x, y: x * y], 
+        3: ["/", lambda x, y: x / y]
+    }
+
+    def __init__(self, opcode=None, *args):
+        self.opcode = opcode
+        self.args = args
+
+    def __repr__(self):
+        if self.opcode in FunctionStatment.operations:
+            op = FunctionStatment.operations[self.opcode][0]
+            return "(%s %s %s)" % (self.args[0], op, self.args[1])
+        return "op%d %s" % (self.opcode, self.args)
+
+    def evaluate(self):
+        op = FunctionStatment.operations[self.opcode][1]
+        arg_values = [arg.evaluate().as_number() for arg in self.args]
+        return NumberStatement(op(*arg_values))
+
+    def read_from_stream(self, stream):
+        count = stream.read_byte()
+        self.args = []
+        for i in range(count):
+            self.args.append(stream.read_statement())
+    
+    def write_to_stream(self, stream):
+        stream.write_byte(len(self.args))
+        for arg in self.args:
+            stream.write_statement(arg)
+
+statement_type = {0 : NilStatement, 1 : NumberStatement, 2 : FunctionStatment}
+id_from_statement = {}
+for key, value in statement_type.iteritems():
+    id_from_statement[value] = key
+
+print "doing stuff"
+my_array = array.array('B',[1,2,3,4, 5,6])
+c = CodeStream(my_array)
+print c.read_statement()
+
+print NumberStatement(52.7)
+fn = FunctionStatment(0, 
+                FunctionStatment(1, NumberStatement(80), NumberStatement(31)),
+                FunctionStatment(2, NumberStatement(2), NumberStatement(4)));
+print fn
+print fn.evaluate()
+
+buf = array.array('B', '\0' * 50)
+d = CodeStream(buf)
+d.write_statement(fn)
+print d
+print d.buffer
