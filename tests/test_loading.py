ad47344a193c257bdfeaca04e8eb39909d4f82cb
Clinton Blackmore <clinton.blackmore@gmail.com>
2014-05-16 16:09:52 -0600

    Now serializing sprite objects

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -8,22 +8,53 @@ except ImportError:
 
 sys.path.append('..')
 from project import Project
+from sprite import Sprite
+import util
+
+sample_document = "sample_project.xml"
 
     
 class PyInterpreterTestCase(unittest.TestCase):
 
-	def test_serialization(self):
-		from project import Project
+	def test_number_to_string_conversion(self):
+		for item in [0, 7, 40, 500, 0.2, 1e5, -14.7]:
+			self.assertEqual(item, 
+				util.number_from_string( 
+					util.number_to_string( item ))) 
 
-		filename = "sample_project.xml"
-		tree = xmltodict.parse(open(filename))
+	def test_boolean_to_string_conversion(self):
+	
+		self.assertEqual(True, util.bool_from_string("true"))
+		self.assertEqual(False, util.bool_from_string("false"))
+		
+		self.assertRaises(ValueError, util.bool_from_string, ("True",))	# no capital!
+		self.assertRaises(ValueError, util.bool_from_string, ("False",))	# no capital!
+		self.assertRaises(ValueError, util.bool_from_string, ("",))	
+		self.assertRaises(ValueError, util.bool_from_string, ("0",))
+		self.assertRaises(ValueError, util.bool_from_string, ("1",))
+		self.assertRaises(ValueError, util.bool_from_string, ("gibberish",))	
+
+		for item in ["true", "false"]:
+			self.assertEqual(item, 
+				util.bool_to_string(
+					util.bool_from_string( item )))
+
+	def test_serialization_of_project(self):
+		tree = xmltodict.parse(open(sample_document))
 		p = Project()
 		p.deserialize(tree)
 		new_tree = p.serialize()
-		xmltodict.unparse(new_tree)		# just to be sure it works
+		#xmltodict.unparse(new_tree)		# just to be sure it works
 
 		self.assertEqual(tree, new_tree)
 
+	def test_serialization_of_sprite(self):
+		tree = xmltodict.parse(open(sample_document))["project"]["stage"]["sprites"]["sprite"][0]
+		s = Sprite()
+		s.deserialize(tree)
+		new_tree = s.serialize()
+		
+		self.assertEqual(tree, new_tree)
 
 if __name__ == '__main__':
     unittest.main()
