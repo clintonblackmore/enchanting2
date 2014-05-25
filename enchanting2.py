20635c60ee3b6eb1970d72b479b1b90bbc02b167
Clinton Blackmore <clinton.blackmore@gmail.com>
2014-05-24 20:49:06 -0600

    Drawing placeholders for sprites.

--- enchanting2.py
+++ enchanting2.py
@@ -4,6 +4,7 @@ This is the main entry point of the system"""
 
 import sys
 import xml.etree.cElementTree as ElementTree
+import pygame
 
 import actor
 
@@ -17,21 +18,37 @@ def main(argv):
 	
 	# Now, we can run the code
 	all_actors = [project.stage]
-	all_actors.extend([sprite for sprite in project.stage.sprites])
+	all_actors.extend([sprite for sprite in project.stage.sprites
+					   if isinstance(sprite, actor.BaseActor)])
+	
+	pygame.init()
+	width = project.stage.width
+	height = project.stage.height
+	screen = pygame.display.set_mode((width, height))
+	
+	white = (255, 255, 255)
+	black = (0, 0, 0)
 	
 	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: 
+				sys.exit()
+		
 		for sprite in all_actors:
-			# Check that it is really a sprite, and not a 'Watcher'
-			# (Ugly!)
-			if isinstance(sprite, actor.BaseActor):
-				print sprite.name
-				for script in sprite.scripts:
-					try:
-						script.step(sprite)
-					except StopIteration:
-						pass
-						
+			for script in sprite.scripts:
+				try:
+					script.step(sprite)
+				except StopIteration:
+					pass
 
-
+		screen.fill(white)
+		for sprite in all_actors:
+			if not isinstance(sprite, actor.Stage):		
+				color = black
+				pos = (int(sprite.x + width / 2), int(height / 2 - sprite.y))
+				radius = 15
+				pygame.draw.circle(screen, color, pos, radius) 
+		pygame.display.flip()
+	
 if __name__ == "__main__":
     main(sys.argv)
