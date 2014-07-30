0a7f87cd0b749191d8a15f578f558b4e2c82a1ac
Clinton Blackmore <clinton.blackmore@gmail.com>
2014-07-29 22:32:54 -0600

    Added Snap! project into web folder
    Added Snap! from my mirror branch at
    https://github.com/clintonblackmore/Snap--Build-Your-Own-Blocks, and
    adjusted the static server to allow for directory listings so that
    getting costumes works right.

--- server.py
+++ server.py
@@ -10,11 +10,37 @@ from geventwebsocket.protocols.wamp import WampProtocol, export_rpc
 def static_file_server(environ, start_response):
 	filename = "web" + environ["PATH_INFO"]
 	mime = "text/html"
+	status = "200 OK"
+	content = None
+	
+	if os.path.isdir(filename):
+		# We need to create a directory listing
+		# One line per file
+		crawl_dir = filename	# local directory we are looking at
+		http_dir = environ["PATH_INFO"].lstrip("/")	# relative URL of directory
+		
+		# special case -- if we are looking at the root directory,
+		# return the snap web page
+		if http_dir == "":
+			filename = "web/snap.html"
+		else:	
+			# If the directory doesn't end in a "/", we have to issue a redirect
+			if not http_dir.endswith("/"):
+				start_response('301 Redirect', [('Location', '/' + http_dir + '/'),])
+				return "301 Moved Permanently"
+			content = "<html><head></head><body><h1>Listing of %s</h1><pre>" % http_dir
+			filelist = os.listdir(crawl_dir)
+			hreflist = ['<a href="%s">%s</a>' % (f, f) for f in filelist]
+			content += "\n".join(hreflist)
+			content += "</pre></body></html>"
+			
 	if os.path.isfile(filename):
-		status = "200 OK"
 		mime = mimetypes.guess_type(filename)
+		if filename.endswith(".js"):
+			mime = "application/javascript"
 		content = open(filename).readlines()
-	else:
+
+	if content == None:
 		status = "404 Not Found"
 		content = "<h1>404 Not found</h1><p>%s does not exist" % (filename)
 
